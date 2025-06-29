cc36d58ceefae3e38d397a87711d855e847fd37e
juanrv27 <rodriguezvelasquezjuanjose11@gmail.com>
2025-06-29 17:55:00 -0500

    Cambiando el api login

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -10,12 +10,16 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.models.User || mongoose.model("User", UserSchema, "Usuarios");
 
 export async function POST(req: Request) {
-  await connectToDatabase();
-  const { email, password } = await req.json();
-  const user = await User.findOne({ email, password });
-  if (user) {
-    return NextResponse.json({ success: true, message: "Login correcto" });
-  } else {
-    return NextResponse.json({ success: false, message: "Credenciales incorrectas" }, { status: 401 });
+  try {
+    await connectToDatabase();
+    const { email, password } = await req.json();
+    const user = await User.findOne({ email, password });
+    if (user) {
+      return NextResponse.json({ success: true, message: "Login correcto" });
+    } else {
+      return NextResponse.json({ success: false, message: "Credenciales incorrectas" }, { status: 401 });
+    }
+  } catch (error) {
+    return NextResponse.json({ success: false, message: "Error en el servidor" }, { status: 500 });
   }
 }
