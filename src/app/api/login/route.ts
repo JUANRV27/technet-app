52a6067a4ba02bd5597d764b896b4451a9019632
juanrv27 <rodriguezvelasquezjuanjose11@gmail.com>
2025-06-29 18:02:16 -0500

    a

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  contrasena: { type: String, required: true }, // Cambiado a "contrasena"
 });
 
 const User = mongoose.models.User || mongoose.model("User", UserSchema, "Usuarios");
@@ -13,6 +13,7 @@ export async function POST(req: Request) {
   try {
     await connectToDatabase();
     const { email, password } = await req.json();
+    // Buscar por "contrasena" en la base de datos
     const user = await User.findOne({ email, contrasena: password });
     if (user) {
       return NextResponse.json({ success: true, message: "Login correcto" });
